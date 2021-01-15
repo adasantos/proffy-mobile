a6f6d6837260c79d3fa2edeb9f5af98b28579dca
Andre Dionisio <andredionisio12@gmail.com>
2021-01-14 21:56:16 -0300

    implementing navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing';
+import AppLoading from 'expo-app-loading';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts} from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
+
 
 export default function App() {
-  return (
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
