8129320dffe07275a176c49137c3657edf5d6fb3
Andre Dionisio <andredionisio12@gmail.com>
2021-01-17 23:47:37 -0300

    created give classes screen

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 
 function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
   return(
-    <View />
+    <View style={styles.container}>
+      <ImageBackground 
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>
+          Quer ser um Proffy?
+        </Text>
+
+        <Text style={styles.description}>
+          Para começar, você preisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>
+          Tudo bem
+        </Text>       
+      </RectButton>
+    </View>
   )
 }
 
