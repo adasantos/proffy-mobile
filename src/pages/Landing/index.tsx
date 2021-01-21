0c45d5c5d4b806975317545b12f8add99b1452e8
Andre Dionisio <andredionisio12@gmail.com>
2021-01-20 23:55:48 -0300

    listing teachers on list and favorites screen

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
-
 import styles from './styles';
+
 function Landing() {
   const { navigate } = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total)
+    })
+  }, [totalConnections])
 
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses')
@@ -54,7 +66,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
 
         <Image source={heartIcon}/>
       </Text>
